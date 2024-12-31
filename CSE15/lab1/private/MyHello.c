e6030f89f6beedded9cc61feb777c62515c4a16c
Eagle Huang <67569990+06huange@users.noreply.github.com>
2024-12-31 17:41:55 +0800

    Add files via upload

---
+++ CSE15/lab1/private/MyHello.c
@@ -0,0 +1,17 @@
+// MyHello.c
+// Prints "Computer Science is Fun!" to stdout
+/*
+* Name:Kuo-Peng Huang
+* UCSC ID:khuang53
+* Class:CSE15
+* Role:Makefile for lab1
+* Assignment:Lab1
+* Date:Oct 10,2019
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#define CS_STRING "Computer Science is Fun!\n"
+int main(){
+ printf(CS_STRING);
+ return EXIT_SUCCESS;
+}
